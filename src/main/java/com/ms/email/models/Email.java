59d520b3b42c6ebb58dfd387871d093b8c4539a5
CarlosEDsl <carloseduardoestds@gmail.com>
2024-05-31 14:04:02 -0300

    Email Service and Repository creation

--- src/main/java/com/ms/email/models/Email.java
+++ src/main/java/com/ms/email/models/Email.java
@@ -1,14 +1,34 @@
 package com.ms.email.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.ms.email.models.enums.StatusEmail;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity
 @Table(name = Email.TABLE_NAME)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Email {
 
     final static String TABLE_NAME = "emailContent";
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID emailId;
 
+    private UUID userId;
+    private String emailFrom;
+    private String emailTo;
+    private String subject;
+    @Column(columnDefinition = "TEXT")
+    private String text;
+    private LocalDateTime sendDateEmail;
+    private StatusEmail statusEmail;
 
 }
